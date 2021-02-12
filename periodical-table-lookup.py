17ea4c7915007bd2fc279a12524893530072b388
af2111 <af@martinfritz.net>
2021-02-12 12:06:37 +0100

    added newline after input

--- periodical-table-lookup.py
+++ periodical-table-lookup.py
@@ -8,7 +8,7 @@ def element_to_string(element: dict):
 
 table_file = open("periodical-table.json", "r")
 table_json: dict = json.load(table_file)
-order_num = input("what's the order number of your element OR search by element name").lower()
+order_num = input("what's the order number of your element OR search by element name\n").lower()
 try:
     order_num = int(order_num)
     if order_num <= 0:
