e44be0ea60e6dbdae38adcb31c3bea266ccb6044
af2111 <af@martinfritz.net>
2021-02-12 12:05:25 +0100

    added search by element name

--- periodical-table-lookup.py
+++ periodical-table-lookup.py
@@ -8,13 +8,22 @@ def element_to_string(element: dict):
 
 table_file = open("periodical-table.json", "r")
 table_json: dict = json.load(table_file)
-order_num = int(input("what's the order number of your element?"))
-if order_num <= 0:
-    print("that's too low.")
-    exit()
+order_num = input("what's the order number of your element OR search by element name").lower()
 try:
-    element_name: list = table_json["order"][order_num]
-    elem_string = element_to_string(table_json[element_name])
-    print(elem_string)
-except IndexError:
-    print("that element doesnt exist loser")
+    order_num = int(order_num)
+    if order_num <= 0:
+        print("that's too low.")
+        exit()
+    try:
+        element_name: list = table_json["order"][order_num]
+        elem_string = element_to_string(table_json[element_name])
+        print(elem_string)
+    except IndexError:
+        print("that element doesnt exist loser")
+except ValueError:
+    try:
+        elem_name = table_json[order_num]
+        elem_string = element_to_string(elem_name)
+        print(elem_string)
+    except KeyError:
+        print("that element doesnt exist loser")
